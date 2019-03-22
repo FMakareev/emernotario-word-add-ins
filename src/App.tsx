960617e78a3acc7f9ced2283c95926ec04edd54a
fmakareev <fmakareev@code-artel.com>
2019-03-22 21:58:37 +0300

    Remove comments. Add AppProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 /* global Office, Excel */
 
 import * as React from 'react';
-// import {ApolloProvider} from 'react-apollo';
-// import apollo from '../modules/apollo';
 
-
-// import Progress from './components/Progress';
-
-import {RouterProvider} from './modules/router';
+import {RouterProvider, RouterConsumer} from './modules/router';
 
 import home from './modules/home/index';
-import auth from './modules/auth/index';
 
-import Link from './modules/router/components/Link/Link';
 import StyledThemeProvider from './styles/StyledThemeProvider';
 import {Layout} from './components/Layout/Layout';
+import {AppProvider} from './store/AppStore';
 
 
 export interface AppProps {
@@ -25,6 +19,15 @@ export interface AppProps {
 export interface AppState {
 
 }
+
+const AppProviderWithRouterConsumer = (props) => {
+
+    return <RouterConsumer>
+        {value => {
+            return <AppProvider {...value} {...props}/>;
+        }}
+    </RouterConsumer>;
+};
 
 
 export default class App extends React.Component<AppProps, AppState> {
@@ -38,41 +41,17 @@ export default class App extends React.Component<AppProps, AppState> {
     }
 
     render() {
-        // const {
-        //     title,
-        //     isOfficeInitialized,
-        // } = this.props;
-
-
         return (
             <RouterProvider
                 routes={[
                     ...home.routes,
-                    ...auth.routes,
                 ]}
             >
-                <StyledThemeProvider>
-                    <React.Fragment>
-
-                        {/*{*/}
-                            {/*!isOfficeInitialized &&*/}
-                            {/*<Progress*/}
-                                {/*title={title}*/}
-                                {/*logo='assets/icon_logo.svg'*/}
-                                {/*message='Please sideload your addin to see app body.'*/}
-                            {/*/>*/}
-                        {/*}*/}
-                        <div>
-                            <Link to={'/'}>
-                                HomePage
-                            </Link>
-                            <Link to={'/login'}>
-                                AuthPage
-                            </Link>
-                            <Layout/>
-                        </div>
-                    </React.Fragment>
-                </StyledThemeProvider>
+                <AppProviderWithRouterConsumer {...this.props}>
+                    <StyledThemeProvider>
+                        <Layout/>
+                    </StyledThemeProvider>
+                </AppProviderWithRouterConsumer>
             </RouterProvider>
         );
     }

210c27a2684a9dc5e32ecb3d9c5b966eee8c6f2f
fmakareev <fmakareev@code-artel.com>
2019-03-27 17:28:32 +0300

    Add validation

--- src/modules/home/view/notaryDataPage/index.tsx
+++ src/modules/home/view/notaryDataPage/index.tsx
@@ -6,76 +6,106 @@ import ButtonBase from '../../../../components/ButtonBase/ButtonBase';
 import {AppConsumer, IAppContextProps} from '../../../../store/AppStore';
 import {Translate} from 'react-localize-redux';
 import Flex from '../../../../components/Flex/Flex';
+import {withLocalize} from 'react-localize-redux';
+import {ILocalize} from '../../../../interfaces/react-localize-redux';
+import {
+    KeyKeeperApi
+} from '../../../../keyKeeperApi';
+
 
 
 export interface IFormRegistrationValues {
-    lastName: string,
-    firstName: string,
-    patronymic: string,
+    lastName?: string,
+    firstName?: string,
+    patronymic?: string,
     certificateNumber?: string,
 }
 
-export interface IFormRegistrationProps {
+export interface IFormRegistrationProps extends ILocalize {
     onSubmit(values: IFormRegistrationValues): Promise<void>,
 
     initialValues?: IFormRegistrationValues,
+
+    [propName: string]: any,
 }
 
-const FormRegistration: React.FC<IFormRegistrationProps> = ({onSubmit, initialValues}) => (<Form
-    onSubmit={onSubmit}
-    initialValues={initialValues}
-    render={({handleSubmit, pristine, invalid}) => (
-        <form style={{width: '100%', maxWidth: '320px'}} onSubmit={handleSubmit}>
-            <Box width={'100%'}>
-                <Box mb={5}>
-                    {/* Фамилия */}
-                    <Translate>
-                        {({translate}) => <Field
-                            name='lastName'
-                            component={TextField}
-                            placeholder={translate('user.lastName')}
-                        />}
-                    </Translate>
-                </Box>
-                <Box mb={5}>
-                    {/* Имя  */}
-                    <Translate>
-                        {({translate}) => <Field
-                            name='firstName'
-                            component={TextField}
-                            placeholder={translate('user.firstName')}
-                        />}
-                    </Translate>
-                </Box>
-                <Box mb={5}>
-                    {/* Отчество */}
-                    <Translate>
-                        {({translate}) => <Field
-                            name='patronymic'
-                            component={TextField}
-                            placeholder={translate('user.patronymic')}
-                        />}
-                    </Translate>
-                </Box>
-                <Box mb={7}>
-                    {/* номер свидетельства */}
-                    <Translate>
-                        {({translate}) => <Field
-                            name='certificateNumber'
-                            component={TextField}
-                            placeholder={translate('notary.certificateNumber')}
-                        />}
-                    </Translate>
-                </Box>
-            </Box>
 
-            <ButtonBase type='submit' disabled={pristine || invalid}>
-                <Translate id={'button.save'}/>
-            </ButtonBase>
-        </form>
-    )}
-/>);
+const validate = (translate: any) => (values: IFormRegistrationValues) => {
+    const errors: IFormRegistrationValues = {};
+    if (!values.firstName) {
+        errors.firstName = translate('validation.required');
+    }
+    if (!values.lastName) {
+        errors.lastName = translate('validation.required');
+    }
+    if (!values.patronymic) {
+        errors.patronymic = translate('validation.required');
+    }
+    if (!values.certificateNumber) {
+        errors.certificateNumber = translate('validation.required');
+    }
 
+    return errors;
+};
+
+const FormRegistration: React.FC<IFormRegistrationProps> =
+    ({onSubmit, initialValues, translate}) => (<Form
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        validate={validate(translate)}
+        render={({handleSubmit, pristine, invalid}) => (
+            <form style={{width: '100%', maxWidth: '320px'}} onSubmit={handleSubmit}>
+                <Box width={'100%'}>
+                    <Box mb={5}>
+                        {/* Фамилия */}
+                        <Translate>
+                            {({translate}) => <Field
+                                name='lastName'
+                                component={TextField}
+                                placeholder={translate('user.lastName')}
+                            />}
+                        </Translate>
+                    </Box>
+                    <Box mb={5}>
+                        {/* Имя  */}
+                        <Translate>
+                            {({translate}) => <Field
+                                name='firstName'
+                                component={TextField}
+                                placeholder={translate('user.firstName')}
+                            />}
+                        </Translate>
+                    </Box>
+                    <Box mb={5}>
+                        {/* Отчество */}
+                        <Translate>
+                            {({translate}) => <Field
+                                name='patronymic'
+                                component={TextField}
+                                placeholder={translate('user.patronymic')}
+                            />}
+                        </Translate>
+                    </Box>
+                    <Box mb={7}>
+                        {/* номер свидетельства */}
+                        <Translate>
+                            {({translate}) => <Field
+                                name='certificateNumber'
+                                component={TextField}
+                                placeholder={translate('notary.certificateNumber')}
+                            />}
+                        </Translate>
+                    </Box>
+                </Box>
+
+                <ButtonBase type='submit' disabled={pristine || invalid}>
+                    <Translate id={'button.save'}/>
+                </ButtonBase>
+            </form>
+        )}
+    />);
+
+const FormRegistrationWithLocalize = withLocalize(FormRegistration);
 
 interface INotaryDataPageProps extends IAppContextProps {
     [propName: string]: any,
@@ -86,11 +116,12 @@ interface INotaryDataPageState {
 }
 
 export class NotaryDataPage extends React.Component<INotaryDataPageProps, INotaryDataPageState> {
+    rpcClient: any = null;
 
     constructor(props) {
         super(props);
         this.state = this.initialState;
-
+        this.rpcClient = new KeyKeeperApi();
     }
 
     get initialState() {
@@ -100,7 +131,8 @@ export class NotaryDataPage extends React.Component<INotaryDataPageProps, INotar
     onSubmit = async (values: IFormRegistrationValues) => {
         console.log(values);
         this.props.setNotaryDataToAppStore(values);
-    };
+    }
+
 
     render() {
         return (<Flex alignItems={'center'} flexDirection={'column'}>
@@ -113,7 +145,8 @@ export class NotaryDataPage extends React.Component<INotaryDataPageProps, INotar
             <Box fontSize={7} lintHeght={9} fontWeight={'bold'} textAlign={'center'} mb={5} maxWidth={'320px'}>
                 <Translate id={'notaryDataPage.notaryDataTitle'}/>
             </Box>
-            <FormRegistration
+
+            <FormRegistrationWithLocalize
                 initialValues={{
                     ...this.props.notary,
                 }}

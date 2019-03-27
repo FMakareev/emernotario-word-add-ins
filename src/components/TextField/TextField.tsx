91921e0bca2e5d72fec1b0a68fc46a5081de0d49
fmakareev <fmakareev@code-artel.com>
2019-03-27 17:23:45 +0300

    Add message

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
+// import styled from 'styled-components';
 
 /** View */
 import {TextFieldBase} from '../TextFieldBase/TextFieldBase';
+import Box from '../Box/Box';
 
 // interface ITextFieldProps {
 //     input?: any,
@@ -9,15 +11,23 @@ import {TextFieldBase} from '../TextFieldBase/TextFieldBase';
 //     [propName: string]: any,
 // }
 
+// const Message = styled()
 
 export class TextField extends React.Component<any> {
 
     render() {
-        const {input} = this.props;
-        return (<TextFieldBase
-            {...this.props}
-            {...input}
-        />);
+        const {input, meta} = this.props;
+        return (<Box>
+            <Box mb={3}>
+                <TextFieldBase
+                    {...this.props}
+                    {...input}
+                />
+            </Box>
+            {
+                meta.error && meta.touched && <Box fontSize={5} lineHeight={'24px'} color={'color13'}>{meta.error}</Box>
+            }
+        </Box>);
     }
 }
 

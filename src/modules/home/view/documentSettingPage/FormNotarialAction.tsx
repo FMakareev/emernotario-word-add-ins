d1f0e514c56d45fd0835fa487688e94ee86274ab
fmakareev <fmakareev@code-artel.com>
2019-03-27 17:27:51 +0300

    Change form

--- src/modules/home/view/documentSettingPage/FormNotarialAction.tsx
+++ src/modules/home/view/documentSettingPage/FormNotarialAction.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {Form, Field} from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
-import {Translate} from 'react-localize-redux';
+import {withLocalize} from 'react-localize-redux';
 
 import {FieldArray} from 'react-final-form-arrays';
 import TextField from '../../../../components/TextField/TextField';
@@ -9,6 +9,9 @@ import Box from '../../../../components/Box/Box';
 import ButtonBase from '../../../../components/ButtonBase/ButtonBase';
 import Flex from '../../../../components/Flex/Flex';
 import {IFormRegistrationValues} from '../notaryDataPage';
+import {ILocalize} from '../../../../interfaces/react-localize-redux';
+import DataOfParticipantOfTheNotarialAct from './DataOfParticipantOfTheNotarialAct';
+import {validateFormNotarialActionUser} from './validation';
 
 
 export interface IFormNotarialActionUser extends IFormRegistrationValues {
@@ -26,11 +29,13 @@ export interface IFormNotarialActionValues {
 }
 
 
-export interface IFormNotarialActionProps {
+export interface IFormNotarialActionProps extends ILocalize {
     onSubmit(values: IFormNotarialActionValues): Promise<void>,
+
     onSaveDocument(values: IFormNotarialActionValues): void,
 
     initialValues?: IFormNotarialActionValues,
+
     [propName: string]: any,
 }
 
@@ -42,152 +47,99 @@ export interface IDataOfParticipantOfTheNotarialActProps {
     [propName: string]: any,
 }
 
-const DataOfParticipantOfTheNotarialAct: React.FC<IDataOfParticipantOfTheNotarialActProps> = ({index, name, fields}) => (
-    <Flex key={name + index}>
-        <Box width={'100%'}>
-            <Box mb={5}>
-                {/* Фамилия */}
-                <Translate>
-                    {({translate}) => <Field
-                        name={`${name}.lastName`}
-                        component={TextField}
-                        placeholder={translate('user.lastName')}
-                    />}
-                </Translate>
-            </Box>
-            <Box mb={5}>
-                {/* Имя  */}
-                <Translate>
-                    {({translate}) => <Field
-                        name={`${name}.firstName`}
-                        component={TextField}
-                        placeholder={translate('user.firstName')}
-                    />}
-                </Translate>
-            </Box>
-            <Box mb={5}>
-                {/* Отчество */}
-                <Translate>
-                    {({translate}) => <Field
-                        name={`${name}.patronymic`}
-                        component={TextField}
-                        placeholder={translate('user.patronymic')}
-                    />}
-                </Translate>
-            </Box>
-            <Box mb={7}>
-                {/* ИНН */}
-                <Translate>
-                    {({translate}) => <Field
-                        name={`${name}.tin`}
-                        component={TextField}
-                        placeholder={translate('user.tin')}
-                    />}
-                </Translate>
-            </Box>
-        </Box>
-        <Box
-            pl={5}
-            onClick={() => fields.remove(index)}
-            style={{cursor: 'pointer'}}
-        >
-            ❌
-        </Box>
-    </Flex>
-);
 
 
-export const FormNotarialAction: React.FC<IFormNotarialActionProps> = ({onSubmit, onSaveDocument, initialValues}) => (<Form
-    onSubmit={onSubmit}
-    initialValues={initialValues}
-    mutators={{
-        ...arrayMutators
-    }}
-    render={({
-                 handleSubmit,
-                 pristine,
-                 invalid,
-                 form: {
-                     mutators: {push},
-                 },
-                 values,
-             }) => (
-        <form style={{width: '100%', maxWidth: '320px'}} onSubmit={handleSubmit}>
 
-            <Box mb={9}>
-                <FieldArray name='customers'>
-                    {
-                        ({fields}) =>
-                            fields.map((name, index) => <DataOfParticipantOfTheNotarialAct
-                                index={index}
-                                name={name}
-                                fields={fields}
-                            />)
-                    }
-                </FieldArray>
 
-                <ButtonBase type='button' onClick={() => push('customers', undefined)}>
-                    <Translate id={'button.addAMember'}/>
-                </ButtonBase>
-            </Box>
+export const FormNotarialAction: React.FC<IFormNotarialActionProps> = ({onSubmit, onSaveDocument, initialValues, translate}) => (
+    <Form
+        onSubmit={onSubmit}
+        initialValues={initialValues}
+        validate={validateFormNotarialActionUser(translate)}
+        mutators={{
+            ...arrayMutators
+        }}
+        render={({
+                     handleSubmit,
+                     // pristine,
+                     invalid,
+                     form: {
+                         mutators: {push},
+                     },
+                     values,
+                 }) => (
+            <form style={{width: '100%', maxWidth: '320px'}} onSubmit={handleSubmit}>
 
-            <Box>
-                <Translate>
-                    {({translate}) => <>
-                        <Box mb={5}>
-                            {/* номер в реестре */}
-                            <Field
-                                name='numberInTheRegistry'
-                                component={TextField}
-                                placeholder={translate('document.numberInTheRegistry')}
-                            />
-                        </Box>
-                        <Box mb={5}>
-                            {/* Дата документа (по умолчанию текущая) */}
-                            <Field
-                                name='documentDate'
-                                component={TextField}
-                                placeholder={translate('document.documentDate')}
-                            />
-                        </Box>
-                        <Box mb={5}>
-                            {/* Действующий до (если есть) */}
-                            <Field
-                                name='existingTo'
-                                component={TextField}
-                                placeholder={translate('document.existingTo')}
-                            />
-                        </Box>
+                <Box mb={9}>
+                    <FieldArray name='customers'>
+                        {
+                            ({fields}) =>
+                                fields.map((name, index) => <DataOfParticipantOfTheNotarialAct
+                                    index={index}
+                                    name={name}
+                                    fields={fields}
+                                    key={name + index}
+                                />)
+                        }
+                    </FieldArray>
 
-                        <Box mb={5}>
-                            {/* Тип документа */}
-                            <Field
-                                name='documentType'
-                                component={TextField}
-                                placeholder={translate('document.documentType')}
-                            />
-                        </Box>
-                    </>}
-                </Translate>
-
-            </Box>
-            <Flex width={'100%'} justifyContent={'space-between'}>
-                <Box pr={'12px'}>
-                    <ButtonBase type='button' onClick={() => {
-                        onSaveDocument(values);
-                    }}>
-                        <Translate id={'button.save'}/>
+                    <ButtonBase type='button' onClick={() => push('customers', undefined)}>
+                        {translate('button.addAMember')}
                     </ButtonBase>
                 </Box>
+
                 <Box>
-                    <ButtonBase type='submit' disabled={pristine || invalid}>
-                        <Translate id={'button.createQRCode'}/>
-                    </ButtonBase>
+                    <Box mb={5}>
+                        {/* номер в реестре */}
+                        <Field
+                            name='numberInTheRegistry'
+                            component={TextField}
+                            placeholder={translate('document.numberInTheRegistry')}
+                        />
+                    </Box>
+                    <Box mb={5}>
+                        {/* Дата документа (по умолчанию текущая) */}
+                        <Field
+                            name='documentDate'
+                            component={TextField}
+                            placeholder={translate('document.documentDate')}
+                        />
+                    </Box>
+                    <Box mb={5}>
+                        {/* Действующий до (если есть) */}
+                        <Field
+                            name='existingTo'
+                            component={TextField}
+                            placeholder={translate('document.existingTo')}
+                        />
+                    </Box>
+
+                    <Box mb={5}>
+                        {/* Тип документа */}
+                        <Field
+                            name='documentType'
+                            component={TextField}
+                            placeholder={translate('document.documentType')}
+                        />
+                    </Box>
                 </Box>
-            </Flex>
-        </form>
-    )}
-/>);
+                <Flex width={'100%'} justifyContent={'space-between'}>
+                    <Box pr={'12px'}>
+                        <ButtonBase type='button' onClick={() => {
+                            onSaveDocument(values);
+                        }}>
+                            {translate('button.save')}
+                        </ButtonBase>
+                    </Box>
+                    <Box>
+                        <ButtonBase type='submit' disabled={invalid}>
+                            {translate('button.createQRCode')}
+                        </ButtonBase>
+                    </Box>
+                </Flex>
+            </form>
+        )}
+    />);
 
 
-export default FormNotarialAction;
+export default withLocalize(FormNotarialAction);

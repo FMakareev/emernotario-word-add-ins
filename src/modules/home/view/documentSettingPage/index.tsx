66a734b23ee9fb44c3a971fe47c17da75efa06cd
fmakareev <fmakareev@code-artel.com>
2019-03-27 17:28:49 +0300

    Add RouterConsumer

--- src/modules/home/view/documentSettingPage/index.tsx
+++ src/modules/home/view/documentSettingPage/index.tsx
@@ -4,6 +4,7 @@ import Box from '../../../../components/Box/Box';
 import {AppConsumer, IAppContextProps} from '../../../../store/AppStore';
 import {Translate} from 'react-localize-redux';
 import Flex from '../../../../components/Flex/Flex';
+import {RouterConsumer} from '../../../router';
 
 
 interface INotarialActionPageProps extends IAppContextProps {
@@ -18,7 +19,14 @@ interface INotarialActionPageState {
 export class DocumentSettingPage extends React.Component<INotarialActionPageProps, INotarialActionPageState> {
 
     onSubmit = async (values: IFormNotarialActionValues) => {
-        console.log(values);
+        console.log('onSubmit: ', values, this.props);
+
+        await this.props.setNotarizationDataDataToAppStore(values);
+
+        console.log('onSubmit: ', values, this.props);
+        if (!this.props.notary) {
+            this.props.action.push('/');
+        }
     };
 
     onSaveDocument = (values: IFormNotarialActionValues) => {
@@ -29,14 +37,18 @@ export class DocumentSettingPage extends React.Component<INotarialActionPageProp
         const {notarizationData} = this.props;
         return (<Flex alignItems={'center'} flexDirection={'column'}>
             {/*<Box textAlign={'center'}  fontSize={5} lintHeght={7} mb={9} maxWidth={'320px'}>*/}
-                {/*<Translate id={'notaryAction'}>*/}
-                    {/*Виділіть слово та натисніть на відповідне поле для заповнення*/}
-                {/*</Translate>*/}
+            {/*<Translate id={'notaryAction'}>*/}
+            {/*Виділіть слово та натисніть на відповідне поле для заповнення*/}
+            {/*</Translate>*/}
             {/*</Box>*/}
             <Box fontSize={7} lintHeght={9} fontWeight={'bold'} textAlign={'center'} mb={5} maxWidth={'320px'}>
                 <Translate id={'documentPage.dataOfParticipantsOfTheNotarialAct'}/>
             </Box>
-            <FormNotarialAction initialValues={notarizationData} onSaveDocument={this.onSaveDocument} onSubmit={this.onSubmit}/>
+            <FormNotarialAction
+                initialValues={notarizationData}
+                onSaveDocument={this.onSaveDocument}
+                onSubmit={this.onSubmit}
+            />
         </Flex>);
     }
 }
@@ -50,4 +62,15 @@ const DocumentSettingPageWithAppStoreConsumer = (props) => {
         }}
     </AppConsumer>);
 };
-export default DocumentSettingPageWithAppStoreConsumer;
+
+const DocumentSettingPageWithAppStoreConsumerWithRouterConsumer = (props) => {
+
+    return <RouterConsumer>
+        {value => {
+            return <DocumentSettingPageWithAppStoreConsumer {...value} {...props}/>;
+        }}
+    </RouterConsumer>;
+};
+
+
+export default DocumentSettingPageWithAppStoreConsumerWithRouterConsumer;
